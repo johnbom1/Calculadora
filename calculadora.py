c8da0499359763124a8e28faf7df7f02cb14ae48
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-08-09 19:59:03 -0500

    Adicionar as operações básicas .#2

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 number_1 = int(input('Enter your first number:'))
 number_2 = int(input('Enter your second number:'))
 
-print(f'{number_1} + {number_2} = ',number_1 + number_2)
+print(f'{number_1} + {number_2} = ',number_1 + number_2) 
 
 print(f'{number_1} - {number_2} = ',number_1 - number_2)
 
