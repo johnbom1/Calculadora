247c3c126cf8828152c4e0d750b354c28c57cf0f
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-08-11 08:23:39 -0500

    novo cálculo sem executar a aplicação novamente.#4

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,44 @@
-operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
+def calculate():
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    ''')
+
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+
+    if operation == '+':
+        print(f'{number_1} + {number_2} = ',number_1 + number_2) 
+
+    elif operation == '-':
+        print(f'{number_1} - {number_2} = ',number_1 - number_2)
+
+    elif operation == '*':
+        print(f'{number_1} * {number_2} = ',number_1 * number_2)
+
+    elif operation == '/':
+        print(f'{number_1} / {number_2} = ',number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    again()
+
+# Add again()  function to calculate() function again()
+
+def again():
+    calc_again = input('''
+    do you want to calculate again?
+    Please type Y for Yes or N for NO.
 ''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
 
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
-
-if operation == '+':
-    print(f'{number_1} + {number_2} = ',number_1 + number_2) 
-
-elif operation == '-':
-    print(f'{number_1} - {number_2} = ',number_1 - number_2)
-
-elif operation == '*':
-    print(f'{number_1} * {number_2} = ',number_1 * number_2)
-
-elif operation == '/':
-    print(f'{number_1} / {number_2} = ',number_1 / number_2)
-
-else:
-    print('You have not typed a valid operator, please run the program again.')
+calculate()
